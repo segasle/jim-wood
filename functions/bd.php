4e1bf2737c7f8827daad8f52c1ca4db3842c268f
segasle <segasle@gmail.com>
2019-08-03 15:20:37 +0300

    подключение к бд

---
+++ functions/bd.php
@@ -0,0 +1,10 @@
+<?php
+
+global $mysqli;
+if (empty($mysqli)){
+    $mysqli = mysqli_connect('localhost', 'root', 'root', 'jimwood');
+    mysqli_set_charset($mysqli, 'UTF8');
+}
+if (mysqli_connect_errno()){
+    echo 'ошибка в подключении к БД ('.mysqli_connect_errno().')'.mysqli_connect_error();
+}
