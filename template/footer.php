7349c3758e50f0f569ccdc2d45f64a7d7bb51fa1
sergejslepenkov <Segasle@gmail.com>
2019-08-17 10:14:47 +0300

    ew

--- template/footer.php
+++ template/footer.php
@@ -1,11 +1,12 @@
 </div>
-</div>
 <footer>
     <div class="cellar">
 
     </div>
 </footer>
 </div>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
@@ -19,7 +20,7 @@
 <script src="javascrpt/main.js"></script>
 
 <script>
-    $('.carousel').carousel({interval: 8000});
+    //$('.carousel').carousel({interval: 8000});
 
     function sl_test() {
         $('.carousel').on('slid.bs.carousel', function () {
