d834b342556e30f0e178bfcdbf1e5cc48df8c77d
sergejslepenkov <Segasle@gmail.com>
2019-08-26 14:31:31 +0300

    контакты

---
+++ page/contacts.php
@@ -0,0 +1,33 @@
+<?php
+$tel = '+7-965-681-5335';
+$mail = 'jim-owner@yandex.ru';
+
+?>
+<div class="row">
+    <div class="сol-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <h1>Контакты</h1>
+        <p><span class="fatty">Адрес салона:</span></p>
+        <address>
+            <p>г. Москва, Ярославское шоссе 19 строение 1
+                (Крупнейший мебельный дискаунтер «SOLE MALL» - 3 этаж)
+            </p>
+        </address>
+        <p><span class="fatty">Режим работы:</span></p>
+        <p>с 10:00 до 20:00 ежедневно – желательна предварительная договоренность при посещении салона</p>
+        <p><span class="fatty">Контактный телефон:</span></p>
+        <p><a href="tel:<?php echo $tel;?>"><?php echo $tel;?></a></p>
+        <p><span class="fatty">Контактный E-mail:</span></p>
+        <p><a href="mailto:<?php echo $mail;?>"><?php echo $mail;?></a></p>
+        <p><span class="fatty">Адрес производства:</span></p>
+        <address>
+            <p>респ. Чувашия, г. Мариинский посад, Полевая 19
+            </p>
+        </address>
+        <p><span class="fatty">Соц сеть:</span></p>
+        <p>                <a href="https://www.instagram.com/jim_wood_/?igshid=yczjh842drd6" target="_blank" class="fa fa-instagram fa-3x"></a>
+        </p>
+    </div>
+    <div class="сol-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A58ebed8610ca452ff813075e376b8bf988fbdc06c2cbbd1bd289b23446c95512&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+    </div>
+</div>
