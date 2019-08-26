9626ac713ee9c23739f5d0b33f0885e0a3a80eb0
segasle <segasle@gmail.com>
2019-08-26 22:16:54 +0300

    раздел портфолио работы

--- page/projects.php
+++ page/projects.php
@@ -8,3 +8,105 @@
         </p>
     </div>
 </div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/DSCF7176.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Традиционная итальянская классика в большой квартире
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/IMG_1959.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Комплексная мебелировка квартиры в современном стиле
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/IMG_5511.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Современный минимализм в серых тонах
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/cookers.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Кухня-модерн в коттедж
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/IMG_0012.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Современная белая кухня
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row align-items-center padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <img src="/img/antaranyan.jpg" alt="" class="w-100">
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+        <div class="block_text">
+            <div class="block_text-descriptions">
+                <p class="fatty">Традиционная классика и шейкер в одном флаконе
+                </p>
+                <p><a href="">
+                        Подробнее
+                    </a></p>
+            </div>
+        </div>
+    </div>
+</div>
