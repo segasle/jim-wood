9f0949c7fc06dd1cd95317bbd974fdfeadeceb6a
segasle <segasle@gmail.com>
2019-08-28 17:42:24 +0300

    QWET

--- page/pine-cones.php
+++ page/pine-cones.php
@@ -1,1 +1,90 @@
-<?php
+<h1 class="text-center"></h1>
+
+<div class="block_text block-center">
+    <div class="block_text-descriptions">
+        <p><span class="tab fatty"></span>
+        </p>
+    </div>
+</div>
+<div class="row padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7176.jpg" alt="Первый слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7164.jpg" alt="Второй слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7165.jpg" alt="Третий слайд">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <table class="w-100" border="1">
+            <tr>
+                <td>
+                    <p class="text-center">Год завершения проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">2018</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Место проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">г. Чебоксары</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Стиль</p>
+                </td>
+                <td>
+                    <p class="text-center">Итальянская классика</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Тип планировки</p>
+                </td>
+                <td>
+                    <p class="text-center">Параллельна, островная</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
+                </td>
+                <td>
+                    <ul>
+                        <li>фасады: классика, рамочные, крашеные с обеих сторон, выступающая филенка</li>
+                        <li>столешница: кварцевый композит</li>
+                        <li>фартук: мрамор</li>
+                        <li>черная встроенная техника</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Дизайнер</p>
+                </td>
+                <td>
+                    <p class="text-center">Елена Яковлева</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
--- page/sukharkov.php
+++ page/sukharkov.php
@@ -1,1 +1,96 @@
-<?php
+<h1 class="text-center">
+    Комплексная мебелировка квартиры в современном стиле
+</h1>
+
+<div class="block_text block-center">
+    <div class="block_text-descriptions">
+        <p><span class="tab fatty">Современный стиль</span>
+   создан для того, чтобы отдохнуть от поднадоевшей классики, от пафоса и «громоздкости», при этом не теряя уют и гармонию. Главная особенность современной кухни заключается в её практичности и функциональности. Она может быть выглядеть просто и лаконично, но иметь «навороченную» начинку. Внешний вид таких кухонь не подчинен каким-либо дизайнерским канонам оформления, он может сочетать в себе черты нескольких современных стилей: минимализма, модерна, хай-тек, скандинавского, и других.  Главное, чтобы интерьер выглядел стильно и создавал уютную атмосферу «невесомости» пространства. Обычно это ощущение достигается за счет того, что фасады имеют гладкую поверхность – никаких массивных выступающих линий, «тяжелого люкса» и прочих извращенных инсинуаций. Современный стиль, как и всё гениальное, прост. Дверцы открываются от обычного нажатия рукой, имеют либо интегрированные ручки, либо ручки простой геометрической формы.
+
+        </p>
+    </div>
+</div>
+<div class="row padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/sukharkov/IMG_1959.jpg" alt="Первый слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/sukharkov/IMG_1948.jpg" alt="Второй слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/sukharkov/IMG_1982.jpg" alt="Третий слайд">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <table class="w-100" border="1">
+            <tr>
+                <td>
+                    <p class="text-center">Год завершения проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">2019</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Место проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">г. Москва</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Стиль</p>
+                </td>
+                <td>
+                    <p class="text-center">Современная классика</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Тип планировки</p>
+                </td>
+                <td>
+                    <p class="text-center">П-образная с полуостровом</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
+                </td>
+                <td>
+                    <ul>
+                        <li>Фурнитура марки BLUM</li>
+                        <li>фасады: классика, МДФ, матовая эмаль, стиль «шейкер»</li>
+                        <li>столешница: акриловый камень </li>
+                        <li>фартук: плитка «кабанчик»</li>
+                        <li>встроенная техника </li>
+                        <li>монолитная раковина</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Дизайнер</p>
+                </td>
+                <td>
+                    <p class="text-center">Елена Яковлева</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
