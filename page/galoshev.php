50042ea5c41634abc30150f26cc2a9107ef5edde
segasle <segasle@gmail.com>
2019-08-28 14:32:54 +0300

    разделы

--- page/galoshev.php
+++ page/galoshev.php
@@ -14,7 +14,27 @@
 </div>
 <div class="row padding">
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <img src="" alt="" class="w-100">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7176.jpg" alt="Первый слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7164.jpg" alt="Второй слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/galoshev/DSCF7165.jpg" alt="Третий слайд">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
         <table class="w-100" border="1">
