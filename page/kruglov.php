e45588316faaf420c8319889414b7be43cf33462
segasle <segasle@gmail.com>
2019-08-30 11:14:08 +0300

    раздел

--- page/kruglov.php
+++ page/kruglov.php
@@ -1,23 +1,105 @@
-<h1 class="text-center">Традиционная итальянская классика в большой квартире</h1>
+<h1 class="text-center">
+    Современный минимализм в серых тонах
+</h1>
 
 <div class="block_text block-center">
     <div class="block_text-descriptions">
-        <p><span class="tab">ЗИтальянский</span> стиль в интерьере – самое органичное сочетание домашнего уюта и античной роскоши. В итальянском стиле кухонь обычно используются природные материалы (чаще всего – дерево и камень) или стилизованная под них современная отделка. Особенностью фасадов является их внешняя «массивность», которая достигается путём применения декоративных элементов: резьбы, пилястров, филёнок, а также разнообразной лепнины. Кухни, выполненные в итальянском стиле, демонстрируют прочность и долговечность конструкций. Это статусная мебель, которая производит хорошее впечатление. Посмотрите на фото ниже и убедитесь в этом сами.
+        <p><span class="tab">Минимализм</span>
+             – это один из самых распространенных современных стилей интерьера. Этимология его названия указывает на латинское слово minimus, что в переводе означает «наименьший», «минимальный». Действительно, интерьер в стиле минимализма сочетает в себе минимум цветов, фактур, форм и предметов. Многие психологи уверены: чтобы успокоиться, расслабиться или сосредоточиться, необходимо погрузить себя в пространство с минимумом отвлекающих факторов. Как же порой важно после тяжелого дня прийти домой и сразу почувствовать обновление души, «обнуление» эмоций! Кухня в стиле минимализм – это не скучно, не чопорно, не пафосно. Это всегда благородно и со вкусом. Разве можно придраться к кухне, в которой идеально соблюдается триединство цвета, формы и текстуры? Обратите внимание: гладкие поверхности, прямые линии, точные формы, - и всё это выдержано в одной цветовой гамме. Если в интерьере вам больше по душе спокойствие и упорядоченность, ознакомьтесь с нашим замечательным проектом в стиле минимализм!
+
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <img src="" alt="" class="w-100">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/img/kruglov/IMG_5511.jpg" alt="Первый слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/kruglov/IMG_5522.jpg" alt="Второй слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/kruglov/IMG_5537.jpg" alt="Третий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/kruglov/IMG_5570.jpg" alt="Третий слайд">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/img/kruglov/IMG_5575.jpg" alt="Третий слайд">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100">
+        <table class="w-100" border="1">
             <tr>
                 <td>
                     <p class="text-center">Год завершения проекта</p>
                 </td>
                 <td>
-                    <p class="text-center">2018</p>
+                    <p class="text-center">2019</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Место проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">г. Москва</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Стиль</p>
+                </td>
+                <td>
+                    <p class="text-center">Современный минимализм</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Тип планировки</p>
+                </td>
+                <td>
+                    <p class="text-center">П-образная </p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
+                </td>
+                <td>
+                    <ul>
+                        <li>Фурнитура марки BLUM</li>
+                        <li>фасады: классика, МДФ, серая матовая эмаль, утопленная филенка</li>
+                        <li>фартук: камень
+                        </li>
+                        <li>наполнение: ящики и лоток для приборов из массива лерева</li>
+                        <li>встроенная техника из нержавеющей стали</li>
+                        <li>двойная монолитная раковина</li>
+                        <li>открытая сушка для посуды</li>
+                        <li>светодиодная подсветка с датчиком движения по периметру кухни (внутри и снаружи) </li>
+                        <li>большой буфет с вмонтированным холодильником</li>
+                        <li>широкая барная зона</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Дизайнер</p>
+                </td>
+                <td>
+                    <p class="text-center">                                Татьяна Никитина</p>
                 </td>
             </tr>
         </table>
