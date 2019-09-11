0a9fa56bc7ffbd5581746b60c8cd4db3797bdfaa
sergejslepenkov <Segasle@gmail.com>
2019-09-11 15:21:08 +0300

    1

--- page/1.php
+++ page/1.php
@@ -1,7 +1,205 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sergejslepenkov
- * Date: 2019-09-10
- * Time: 15:38
- */
+<a href="?page=projects" class="btn btn-primary">Назад к проектам</a>
+
+<h1 class="text-center h2 fatty">
+    Большая кухня в английском стиле</h1>
+
+<div class="block_text block-center">
+    <div class="block_text-descriptions">
+        <p><span class="fatty"> Английский стиль</span> – это весьма серьезное направление в мире дизайна, зародившееся
+            очень давно,
+            но при этом никогда не имевшее популярность в России. И вот сейчас, когда традиционно приевшийся итальянский
+            стиль постепенно надоедает, люди начинают обустраивать кухни в новом, свежем стиле. Наша фирма “Jim Wood” не
+            отстает от этой тенденции и производит мебельные фасады в английском стиле. Ведь английский стиль в
+            интерьере- это всегда элегантно и респектабельно. Для того чтобы внести ясность, рассмотрим основные
+            особенности кухонь в английском стиле и сделаем вывод о том, какие для них должны быть фасады.
+        </p>
+        <ol class="number">
+            <li><span class="fatty">Строгость и симметричность</span>
+                <ul>
+                    <li>Традиционные английские интерьеры не имеют кривых линий, они просты и изящны, так как англичане
+                        - это очень правильный народ, любящий порядок и отсутствие лишних деталей.
+                    </li>
+                </ul>
+            </li>
+            <li><span class="fatty">Элементы античной культуры.</span>
+                <ul>
+                    <li>
+                        Британский народ очень любит разбавлять свой строгий интерьер декоративными элементами
+                        различного вида: Пилястры, Колонны, Арки, Балюстрады. Ведь каждый человек хочет добавить в свой
+                        интерьер капельку изысканности.
+                    </li>
+                </ul>
+            </li>
+            <li><span class="fatty"> Плотность планировки.</span>
+                <ul>
+                    <li>Мебель в английском стиле спроектирована так, чтобы максимально заполнить пустые места, потому
+                        что британский менталитет таков, что они хотят использовать свои ресурсы максимально эффективно,
+                        что, разумеется, сказалась и в их домах.
+                    </li>
+                </ul>
+            </li>
+            <li><span class="fatty">Дорогие материалы.</span>
+                <ul>
+                    <li>Англичане никогда не скупились на материалы для обустройства своего дома, выбирая только лучшее.
+                        Это, конечно, подразумевает под собой Фасады из Массива дерева. К счастью, технологии не стоят
+                        на месте и для снижения стоимости фасадов, наша фирма может предложить вам Фасады, покрытые
+                        шпоном, что визуально будет выглядеть абсолютно так же элегантно.
+                    </li>
+                </ul>
+            </li>
+        </ol>
+    </div>
+</div>
+<div class="row padding">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <a data-fancybox="gallery" href="/img/1/_DSC0128.jpg">
+            <img class="img-fluid" src="/img/1/_DSC0128.jpg">
+        </a>
+        <div class="row">
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0001(1).jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0001(1).jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0002(1).jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0002(1).jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0015.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0015.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0024.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0024.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0033.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0033.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0147.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0147.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0148.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0148.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0149.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0149.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0157.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0157.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0158.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0158.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0162.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0162.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0059.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0059.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0110.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0110.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0121.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0121.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0133.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0133.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0141.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0141.jpg">
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
+                <a data-fancybox="gallery" href="/img/1/_DSC0143.jpg">
+                    <img class="img-fluid" src="/img/1/_DSC0143.jpg">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <table class="w-100" border="1">
+            <tr>
+                <td>
+                    <p class="text-center">Год завершения проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">2019</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Место проекта</p>
+                </td>
+                <td>
+                    <p class="text-center">г. Москва</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Стиль</p>
+                </td>
+                <td>
+                    <p class="text-center">Современная классика с элементами шейкер</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Тип планировки</p>
+                </td>
+                <td>
+                    <p class="text-center">Угловая с островом</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
+                </td>
+                <td>
+                    <ul>
+                        <li>фасады: МДФ, крашеные с обеих сторон зеленой матовой эмалью, фасады в стиле шейкер</li>
+                        <li>столешница: гранит</li>
+                        <li>фартук: стеклянная плитка</li>
+                        <li>имеется 2 монолитных раковины: двойная в основном корпусе и одинарная в острове</li>
+                        <li>встроенная техника из нержавеющей стали</li>
+                        <li>Фурнитура марки BLUM</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="text-center">Дизайнер</p>
+                </td>
+                <td>
+                    <p class="text-center">Елена Яковлева</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
