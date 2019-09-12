590c947912d99e29b9118c3aa346ac81d50c926d
sergejslepenkov <Segasle@gmail.com>
2019-09-12 11:14:01 +0300

    подвал исправления

--- template/footer.php
+++ template/footer.php
@@ -10,21 +10,26 @@ $mail = 'jim-owner@yandex.ru';
                     ТЦ "Sole Mall" 3 этаж
                 </address>
             </div>
-            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+            <div class="col-8 col-sm-8 col-md-6 col-lg-4 col-xl-4">
                 <div class="block-center">
 
-                    <div>
-                        <a href="tel:<?php echo $tel;?>"><?php echo $tel;?></a>
-                    </div><div>
-                        <a href="mailto:<?php echo $mail;?>"><?php echo $mail;?></a>
+                    <div class="cellar-phone">
+                        <a href="tel:<?php echo $tel; ?>"><?php echo $tel; ?></a>
+                    </div>
+                    <div class="cellar-mail">
+                        <a href="mailto:<?php echo $mail; ?>"><?php echo $mail; ?></a>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 align-self-center">
-                <a href="https://www.instagram.com/jim_wood_/?igshid=yczjh842drd6" target="_blank" class="fa fa-instagram fa-3x pull-right"></a>
+            <div class="col-4 col-sm-4 col-md-6 col-lg-4 col-xl-4">
+                <a href="https://www.instagram.com/jim_wood_/?igshid=yczjh842drd6" target="_blank"
+                   class="fa fa-instagram fa-3x pull-right"></a>
             </div>
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <p style="font-size: 13px; color: black;">Сайт сделан разработчиком <a href="https://ssd18.ru" style="text-decoration-line: underline; color: black;" target="_blank">Сергеем Слепенковым</a></p>
+                <p style="font-size: 13px; color: black;">Сайт сделан разработчиком <a href="https://ssd18.ru"
+                                                                                       style="text-decoration-line: underline; color: black;"
+                                                                                       target="_blank">Сергеем
+                        Слепенковым</a></p>
             </div>
         </div>
     </div>
@@ -38,8 +43,8 @@ $mail = 'jim-owner@yandex.ru';
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js" 
-        integrity="sha256-+4KHeBj6I8jAKAU8xXRMXXlH+sqCvVCoK5GAFkmb+2I=" 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"
+        integrity="sha256-+4KHeBj6I8jAKAU8xXRMXXlH+sqCvVCoK5GAFkmb+2I="
         crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
