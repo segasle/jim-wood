dc5b19ddc49ad1fc8c9640710057ffdfbf332719
segasle <segasle@gmail.com>
2019-09-12 12:51:44 +0300

    таблица

--- template/instagram.php
+++ template/instagram.php
@@ -2,15 +2,15 @@
     <div class="row">
         <div class="col-12 col-sm-12 col-md-6 col-col-lg-10 col-xl-11">
             <div style="border-right: 2px solid black;">
-                <p><span style="font-size: 18pt;"><strong>ПОДПИШИСЬ НА НАС В INSTAGRAM</strong></span></p>
+                <p class="text-center"><span style="font-size: 18pt;"><strong>ПОДПИШИСЬ НА НАС В INSTAGRAM</strong></span></p>
                 <p style="padding-left: 40px;"><span
                             style="font-size: 12pt;">Наши проекты, &nbsp;новости и многое другое</span>
                 </p>
             </div>
         </div>
         <div class="col-12 col-sm-12 col-md-6 col-col-lg-2 col-xl-1">
-            <a href="https://www.instagram.com/jim_wood_/?igshid=fr29k2yhl5sg" class="fa-instagram fa fa-3x"></a>
-
+            <p class="text-center">            <a href="https://www.instagram.com/jim_wood_/?igshid=fr29k2yhl5sg" class="fa-instagram fa fa-3x"></a>
+            </p>
         </div>
     </div>
 </div>
