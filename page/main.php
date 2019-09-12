aec1d4f5d845ff807a47e980dc3783e7782644a4
sergejslepenkov <Segasle@gmail.com>
2019-09-12 10:17:30 +0300

    исправления карусели

--- page/main.php
+++ page/main.php
@@ -2,45 +2,47 @@
     <div id="carousel-main" class="carousel slide carousel-main" data-ride="carousel" data-interval="false">
         <div class="carousel-inner">
             <div class="carousel-item first main-item active">
-                <div class="col-md-5 d-none d-md-block float-left">
-                    <div class="carousel-text pl-3 pr-3">
-                        <p class="h2 fatty">Кухни на заказ в любом стиле</p>
-                        <p>А также корпусная мебель в гостиную, прихожую, ванную, гардероб</p>
-                        <a href="?page=projects" class="w-100 btn btn-outline-primary">Посмотреть портфолио</a>
+                <div class="row">
+                    <div class="col-md-5 col-12 order-xs-1 order-sm-1 d-md-block col-sm-12">
+                        <div class="carousel-text pl-3 pr-3">
+                            <p class="h2 fatty">Кухни на заказ в любом стиле</p>
+                            <p>А также корпусная мебель в гостиную, прихожую, ванную, гардероб</p>
+                            <a href="?page=projects" class="w-100 btn btn-outline-primary">Посмотреть портфолио</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-7 col-sm-12 col-12 float-right">
-                    <div id="carousel-one" class="carousel slide carousel-one" data-ride="carousel"
-                         data-interval="3000">
-                        <div class="carousel-inner">
-                            <div class="carousel-item first-inner active">
-                                <div class="img">
-                                    <img class="d-block img-fluid" src="/img/galoshes.jpg"
-                                         alt="slide">
+                    <div class="col-md-7 col-sm-12 col-12 order-xs-0 order-sm-0">
+                        <div id="carousel-one" class="carousel slide carousel-one" data-ride="carousel"
+                             data-interval="3000">
+                            <div class="carousel-inner">
+                                <div class="carousel-item first-inner active">
+                                    <div class="img">
+                                        <img class="d-block img-fluid" src="/img/galoshes.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="carousel-item first-inner">
-                                <div class="img">
-                                    <img class="d-block img-fluid" src="/img/sukharkov.jpg"
-                                         alt="slide">
+                                <div class="carousel-item first-inner">
+                                    <div class="img">
+                                        <img class="d-block img-fluid" src="/img/sukharkov.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="carousel-item first-inner">
-                                <div class="img">
-                                    <img class="d-block img-fluid" src="/img/Mitrofanova.jpg"
-                                         alt="slide">
+                                <div class="carousel-item first-inner">
+                                    <div class="img">
+                                        <img class="d-block img-fluid" src="/img/Mitrofanova.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="carousel-item first-inner">
-                                <div class="img">
-                                    <img class="d-block img-fluid" src="/img/tub.jpg"
-                                         alt="slide">
+                                <div class="carousel-item first-inner">
+                                    <div class="img">
+                                        <img class="d-block img-fluid" src="/img/tub.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="carousel-item first-inner">
-                                <div class="img">
-                                    <img class="d-block img-fluid" src="/img/hallway.jpg"
-                                         alt="slide">
+                                <div class="carousel-item first-inner">
+                                    <div class="img">
+                                        <img class="d-block img-fluid" src="/img/hallway.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -48,39 +50,44 @@
                 </div>
             </div>
             <div class="carousel-item second main-item">
-                <div class="col-md-5 d-none d-md-block float-left">
-                    <div class="carousel-text pl-3 pr-3">
-                        <p class="h2 fatty">Молодой бренд с богатым прошлым и грандиозными планами</p>
-                        <p>Высокие технологии производства, индивидуальный подход к клиенту, регулярная обратная
-                            связь</p>
-                        <a href="?page=about" class="w-100 btn btn-outline-primary">Узнать больше о Jim Wood</a>
+                <div class="row">
+                    <div class="col-md-5 col-12 order-xs-1 d-md-block order-sm-1">
+                        <div class="carousel-text pl-3 pr-3">
+                            <p class="h2 fatty">Молодой бренд с богатым прошлым и грандиозными планами</p>
+                            <p>Высокие технологии производства, индивидуальный подход к клиенту, регулярная обратная
+                                связь</p>
+                            <a href="?page=about" class="w-100 btn btn-outline-primary">Узнать больше о Jim Wood</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-7 col-sm-12 col-12 float-right">
-                    <div class="img">
-                        <img class="d-block w-100" src="/img/IMG_5771.jpg" alt="Второй слайдер">
+                    <div class="col-md-7 col-sm-12 col-12 order-sm-0 order-xs-0">
+                        <div class="img">
+                            <img class="d-block w-100" src="/img/IMG_5771.jpg" alt="Второй слайдер">
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="carousel-item main-item third">
-                <div class="col-md-5 d-none d-md-block float-left">
-                    <div class="carousel-text pl-3 pr-3">
-                        <p class="h2 fatty7">Как нас найти? </p>
-                        <p>Мы находимся в Москве, производство сосредоточено в республике Чувашия. Заходите в наш
-                            Instagram!</p>
-                        <a href="?page=contacts" class="w-100 btn btn-outline-primary">Подробная контактная информация</a>
+                <div class="row">
+                    <div class="col-md-5 col-12 order-xs-1 d-md-block order-sm-1">
+                        <div class="carousel-text pl-3 pr-3">
+                            <p class="h2 fatty">Как нас найти? </p>
+                            <p>Мы находимся в Москве, производство сосредоточено в республике Чувашия. Заходите в наш
+                                Instagram!</p>
+                            <a href="?page=contacts" class="w-100 btn btn-outline-primary">Подробная контактная
+                                информация</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-7 col-sm-12 col-12 float-right">
-                    <div id="carousel-three" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item three-inner active">
-                                <div class="img">
-                                    <img class="d-block img-fluid w-100" src="/img/maps.jpg"
-                                         alt="slide">
+                    <div class="col-md-7 col-sm-12 col-12 order-xs-0 order-sm-0">
+                        <div id="carousel-three" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item three-inner active">
+                                    <div class="img">
+                                        <img class="d-block img-fluid w-100" src="/img/maps.jpg"
+                                             alt="slide">
+                                    </div>
                                 </div>
-                            </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -92,7 +99,8 @@
             <span class="sr-only">prev</span>
         </a>
 
-        <a class="carousel-control-next" href="#carousel-main" role="button" data-slide="next" onclick="main_right()" style="right: -10px;">
+        <a class="carousel-control-next" href="#carousel-main" role="button" data-slide="next" onclick="main_right()"
+           style="right: -10px;">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">next</span>
         </a>
