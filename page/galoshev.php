8b9da550a7d9e1a4770cb32e043d867f6952ea92
sergejslepenkov <Segasle@gmail.com>
2019-09-13 15:33:50 +0300

    таблица

--- page/galoshev.php
+++ page/galoshev.php
@@ -22,176 +22,202 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7164.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7164.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7164.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7165.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7165.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7165.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7171.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7171.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7171.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7185.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7185.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7185.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7194.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7194.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7194.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7196.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7196.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7196.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7210.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7210.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7210.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7214.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7214.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7214.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7218.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7218.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7218.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7222.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7222.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7222.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7235.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7235.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7235.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7238.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7238.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7238.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7243.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7243.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7243.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7258.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7258.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7258.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7263.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7263.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7263.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7275.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7275.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7275.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7266.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7266.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7266.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7267.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7267.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7267.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7272.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7272.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7272.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7212.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7212.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7212.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7215.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7215.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7215.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7246.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7246.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7246.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/galoshev/DSCF7252.jpg">
-                  <img class="img-fluid" src="/img/galoshev/DSCF7252.jpg">
+                    <img class="img-fluid" src="/img/galoshev/DSCF7252.jpg">
                 </a>
             </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100 table table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl" border="1">
-            <tr>
-                <td>
-                    <p class="text-center">Год завершения проекта</p>
-                </td>
-                <td>
-                    <p class="text-center">2018</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Место проекта</p>
-                </td>
-                <td>
-                    <p class="text-center">г. Чебоксары</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Стиль</p>
-                </td>
-                <td>
-                    <p class="text-center">Итальянская классика</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Тип планировки</p>
-                </td>
-                <td>
-                    <p class="text-center">Параллельна, островная</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
-                    <ul>
-                        <li>фасады: классика, рамочные, крашеные с обеих сторон, выступающая филенка</li>
-                        <li>столешница: кварцевый композит</li>
-                        <li>фартук: мрамор</li>
-                        <li>черная встроенная техника</li>
-                    </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
-                    <p class="text-center">Елена Яковлева</p>
-                </td>
-            </tr>
-        </table>
+        <div class="w-100 table">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Год завершения проекта</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center">2018</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Место проекта</p>
+
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center">г. Чебоксары</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Стиль</p>
+
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center">Итальянская классика</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Тип планировки</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center">Параллельна, островная</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Ключевые особенности и задействованные материалы</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <ul>
+                            <li>фасады: классика, рамочные, крашеные с обеих сторон, выступающая филенка</li>
+                            <li>столешница: кварцевый композит</li>
+                            <li>фартук: мрамор</li>
+                            <li>черная встроенная техника</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center fatty">Дизайнер</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="border">
+                        <p class="text-center">Елена Яковлева</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
