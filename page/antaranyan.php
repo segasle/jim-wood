1c2cbba6bde6c28c050510f5c593cc351128aa0d
sergejslepenkov <Segasle@gmail.com>
2019-09-13 22:11:46 +0300

    таблица

--- page/antaranyan.php
+++ page/antaranyan.php
@@ -3,7 +3,15 @@
 
 <div class="block_text block-center">
     <div class="block_text-descriptions">
-        <p><span class="tab">Совмещение</span>  разных стилей в своей кухне – еще одно подтверждение того, что создание интерьера – это не что иное, как воплощение в жизнь любых фантазий и желаний, настоящее творчество! Представленный ниже проект объединяет в себе элементы двух почти противоположных стилей: изящной традиционной классики и современного шейкер. Это именно тот случай, когда в интерьере всего в меру. Отделка лицевых панелей кухни имитирует позолоту, что сочетается с внешним интерьером и придает кухне помпезности, грации. Особенно успешно с этой задачей справляются перламутровые ручки фасадов и позолоченный кран. В то же время, в кухне присутствуют и деревенские мотивы, они создают общий вид простого, добротного и прагматичного дизайна. Достаточно заметить, что древесным текстурам в этом интерьере отводится особое место. Именно дерево способно «приглушить» пафос и придать кухне спокойный стильный и функциональный вид.
+        <p><span class="tab">Совмещение</span> разных стилей в своей кухне – еще одно подтверждение того, что создание
+            интерьера – это не что иное, как воплощение в жизнь любых фантазий и желаний, настоящее творчество!
+            Представленный ниже проект объединяет в себе элементы двух почти противоположных стилей: изящной
+            традиционной классики и современного шейкер. Это именно тот случай, когда в интерьере всего в меру. Отделка
+            лицевых панелей кухни имитирует позолоту, что сочетается с внешним интерьером и придает кухне помпезности,
+            грации. Особенно успешно с этой задачей справляются перламутровые ручки фасадов и позолоченный кран. В то же
+            время, в кухне присутствуют и деревенские мотивы, они создают общий вид простого, добротного и прагматичного
+            дизайна. Достаточно заметить, что древесным текстурам в этом интерьере отводится особое место. Именно дерево
+            способно «приглушить» пафос и придать кухне спокойный стильный и функциональный вид.
         </p>
     </div>
 </div>
@@ -15,157 +23,162 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5313.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5313.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5313.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5316.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5316.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5316.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5323.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5323.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5323.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5329.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5329.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5329.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5343.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5343.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5343.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5350.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5350.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5350.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5376.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5376.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5376.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5500.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5500.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5500.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5410.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5410.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5410.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5412.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5412.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5412.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5440.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5440.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5440.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5456.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5456.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5456.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5467.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5467.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5467.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5472.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5472.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5472.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5483.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5483.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5483.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5493.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5493.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5493.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5374.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5374.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5374.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5379.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5379.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5379.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/antaranyan/IMG_5394.jpg">
-                  <img class="img-fluid" src="/img/antaranyan/IMG_5394.jpg">
+                    <img class="img-fluid" src="/img/antaranyan/IMG_5394.jpg">
                 </a>
             </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100 table table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl" border="1">
-            <tr>
-                <td>
-                    <p class="text-center">Год завершения проекта</p>
-                </td>
-                <td>
+        <div class="w-100 table">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Год завершения проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">2019</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Место проекта</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Место проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">г. Москва</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Стиль</p>
-                </td>
-                <td>
-                    <p class="text-center">Современный стиль шейкер с элементами традиционной классики</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Тип планировки</p>
-                </td>
-                <td>
-                    <p class="text-center">П-образная</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
-                    <ul>
-                        <li>фасады: в стиле шейкер, крашеные с обеих сторон, утопленная филенка, окрашены матовой эмалью (смесь двух цветов – белого и голубого) </li>
-                        <li>столешница: акриловый камень</li>
-                        <li>ящики с лотками из массива дуба, ящик для фруктов из дуба, полки из сращенных досок, деревянные подносы (входят в фурнитур наподобие ящиков)</li>
-                        <li>все деревянные поверхности окрашены тиковым маслом для сохранения натурального цвета древесины</li>
-                        <li>одинарная монолитная раковина</li>
-                    </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
-                    <p class="text-center">Анастасия Соловьева</p>
-                </td>
-            </tr>
-        </table>
+                </div>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="text-center">Стиль</p>
+                    </td>
+                    <td>
+                        <p class="text-center">Современный стиль шейкер с элементами традиционной классики</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="text-center">Тип планировки</p>
+                    </td>
+                    <td>
+                        <p class="text-center">П-образная</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="text-center">Ключевые особенности и задействованные материалы</p>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>фасады: в стиле шейкер, крашеные с обеих сторон, утопленная филенка, окрашены матовой
+                                эмалью (смесь двух цветов – белого и голубого)
+                            </li>
+                            <li>столешница: акриловый камень</li>
+                            <li>ящики с лотками из массива дуба, ящик для фруктов из дуба, полки из сращенных досок,
+                                деревянные подносы (входят в фурнитур наподобие ящиков)
+                            </li>
+                            <li>все деревянные поверхности окрашены тиковым маслом для сохранения натурального цвета
+                                древесины
+                            </li>
+                            <li>одинарная монолитная раковина</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="text-center">Дизайнер</p>
+                    </td>
+                    <td>
+                        <p class="text-center">Анастасия Соловьева</p>
+                    </td>
+                </tr>
+            </div>
+        </div>
     </div>
 </div>
