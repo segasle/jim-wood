f1e5b47b8aef536359443f0e675bb6d6bcd6ecea
sergejslepenkov <Segasle@gmail.com>
2019-09-15 10:35:32 +0300

    таблица

--- page/pine-cones.php
+++ page/pine-cones.php
@@ -5,7 +5,16 @@
 
 <div class="block_text block-center">
     <div class="block_text-descriptions">
-        <p><span class="tab fatty">Современный стиль</span> создан для того, чтобы отдохнуть от поднадоевшей классики, от пафоса и «громоздкости», при этом не теряя уют и гармонию. Главная особенность современной кухни заключается в её практичности и функциональности. Она может выглядеть просто и лаконично, но иметь «навороченную» начинку. Внешний вид таких кухонь не подчинен каким-либо дизайнерским канонам оформления, он может сочетать в себе черты нескольких современных стилей: минимализма, модерна, хай-тек, скандинавского, и других.  Главное, чтобы интерьер выглядел стильно и создавал уютную атмосферу «невесомости» пространства. Обычно это ощущение достигается за счет того, что фасады имеют гладкую поверхность – никаких массивных выступающих линий, «тяжелого люкса» и прочих извращенных инсинуаций. Современный стиль, как и всё гениальное, прост. Дверцы открываются от обычного нажатия рукой, имеют либо интегрированные ручки, либо ручки простой геометрической формы.
+        <p><span class="tab fatty">Современный стиль</span> создан для того, чтобы отдохнуть от поднадоевшей классики,
+            от пафоса и «громоздкости», при этом не теряя уют и гармонию. Главная особенность современной кухни
+            заключается в её практичности и функциональности. Она может выглядеть просто и лаконично, но иметь
+            «навороченную» начинку. Внешний вид таких кухонь не подчинен каким-либо дизайнерским канонам оформления, он
+            может сочетать в себе черты нескольких современных стилей: минимализма, модерна, хай-тек, скандинавского, и
+            других. Главное, чтобы интерьер выглядел стильно и создавал уютную атмосферу «невесомости» пространства.
+            Обычно это ощущение достигается за счет того, что фасады имеют гладкую поверхность – никаких массивных
+            выступающих линий, «тяжелого люкса» и прочих извращенных инсинуаций. Современный стиль, как и всё
+            гениальное, прост. Дверцы открываются от обычного нажатия рукой, имеют либо интегрированные ручки, либо
+            ручки простой геометрической формы.
 
         </p>
     </div>
@@ -18,144 +27,135 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0036.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0036.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0036.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0042.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0042.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0042.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0053.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0053.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0053.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0085.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0085.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0085.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0097.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0097.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0097.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0099.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0099.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0099.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0108.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0108.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0108.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0136.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0136.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0136.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0139.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0139.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0139.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0172.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0172.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0172.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0178.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0178.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0178.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0180.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0180.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0180.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0197.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0197.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0197.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0203.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0203.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0203.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0201.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0201.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0201.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/pine-cones/IMG_0083.jpg">
-                  <img class="img-fluid" src="/img/pine-cones/IMG_0083.jpg">
+                    <img class="img-fluid" src="/img/pine-cones/IMG_0083.jpg">
                 </a>
             </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100 table table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl" border="1">
-            <tr>
-                <td>
-                    <p class="text-center">Год завершения проекта</p>
-                </td>
-                <td>
+        <div class="w-100 table">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Год завершения проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">2019</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Место проекта</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Место проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">г. Москва</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Стиль</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Стиль</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Современная классика</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Тип планировки</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">угловая</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Ключевые особенности и задействованные материалы</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <ul>
                         <li>Фурнитура марки BLUM</li>
                         <li>фасады: классика, МДФ, матовая эмаль, утопленная филенка</li>
-                        <li>столешница: акриловый камень </li>
+                        <li>столешница: акриловый камень</li>
                         <li>фартук: стекло</li>
                         <li>встроенная техника из нержавеющей стали</li>
                         <li>накладная раковина</li>
                     </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Дизайнер</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Елена Яковлева</p>
-                </td>
-            </tr>
-        </table>
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
 
