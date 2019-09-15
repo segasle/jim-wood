37fdb4ffabcc8b14066e364aa8e692ea7269d7d8
sergejslepenkov <Segasle@gmail.com>
2019-09-15 10:24:31 +0300

    таблица

--- page/kruglov.php
+++ page/kruglov.php
@@ -6,7 +6,16 @@
 <div class="block_text block-center">
     <div class="block_text-descriptions">
         <p><span class="tab fatty">Минимализм</span>
-             – это один из самых распространенных современных стилей интерьера. Этимология его названия указывает на латинское слово minimus, что в переводе означает «наименьший», «минимальный». Действительно, интерьер в стиле минимализма сочетает в себе минимум цветов, фактур, форм и предметов. Многие психологи уверены: чтобы успокоиться, расслабиться или сосредоточиться, необходимо погрузить себя в пространство с минимумом отвлекающих факторов. Как же порой важно после тяжелого дня прийти домой и сразу почувствовать обновление души, «обнуление» эмоций! Кухня в стиле минимализм – это не скучно, не чопорно, не пафосно. Это всегда благородно и со вкусом. Разве можно придраться к кухне, в которой идеально соблюдается триединство цвета, формы и текстуры? Обратите внимание: гладкие поверхности, прямые линии, точные формы, - и всё это выдержано в одной цветовой гамме. Если в интерьере вам больше по душе спокойствие и упорядоченность, ознакомьтесь с нашим замечательным проектом в стиле минимализм!
+            – это один из самых распространенных современных стилей интерьера. Этимология его названия указывает на
+            латинское слово minimus, что в переводе означает «наименьший», «минимальный». Действительно, интерьер в
+            стиле минимализма сочетает в себе минимум цветов, фактур, форм и предметов. Многие психологи уверены: чтобы
+            успокоиться, расслабиться или сосредоточиться, необходимо погрузить себя в пространство с минимумом
+            отвлекающих факторов. Как же порой важно после тяжелого дня прийти домой и сразу почувствовать обновление
+            души, «обнуление» эмоций! Кухня в стиле минимализм – это не скучно, не чопорно, не пафосно. Это всегда
+            благородно и со вкусом. Разве можно придраться к кухне, в которой идеально соблюдается триединство цвета,
+            формы и текстуры? Обратите внимание: гладкие поверхности, прямые линии, точные формы, - и всё это выдержано
+            в одной цветовой гамме. Если в интерьере вам больше по душе спокойствие и упорядоченность, ознакомьтесь с
+            нашим замечательным проектом в стиле минимализм!
 
         </p>
     </div>
@@ -19,105 +28,97 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5522.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5522.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5522.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5575.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5575.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5575.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5586.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5586.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5586.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5607.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5607.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5607.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5626.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5626.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5626.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5646.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5646.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5646.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5652.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5652.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5652.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5671.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5671.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5671.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5681.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5681.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5681.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5685.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5685.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5685.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5537.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5537.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5537.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/kruglov/IMG_5570.jpg">
-                  <img class="img-fluid" src="/img/kruglov/IMG_5570.jpg">
+                    <img class="img-fluid" src="/img/kruglov/IMG_5570.jpg">
                 </a>
             </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100 table table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl" border="1">
-            <tr>
-                <td>
-                    <p class="text-center">Год завершения проекта</p>
-                </td>
-                <td>
+        <div class="w-100 table">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Год завершения проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">2019</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Место проекта</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Место проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">г. Москва</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Стиль</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Стиль</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Современный минимализм</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Тип планировки</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Тип планировки</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">П-образная </p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Ключевые особенности и задействованные материалы</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <ul>
                         <li>Фурнитура марки BLUM</li>
                         <li>фасады: классика, МДФ, серая матовая эмаль, утопленная филенка</li>
@@ -127,20 +128,18 @@
                         <li>встроенная техника из нержавеющей стали</li>
                         <li>двойная монолитная раковина</li>
                         <li>открытая сушка для посуды</li>
-                        <li>светодиодная подсветка с датчиком движения по периметру кухни (внутри и снаружи) </li>
+                        <li>светодиодная подсветка с датчиком движения по периметру кухни (внутри и снаружи)</li>
                         <li>большой буфет с вмонтированным холодильником</li>
                         <li>широкая барная зона</li>
                     </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
-                    <p class="text-center">                                Татьяна Никитина</p>
-                </td>
-            </tr>
-        </table>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Дизайнер</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center"> Татьяна Никитина</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
