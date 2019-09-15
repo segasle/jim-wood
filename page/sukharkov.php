6d08539971ff716af2c41081f36e1ee3cf21d61a
sergejslepenkov <Segasle@gmail.com>
2019-09-15 10:48:15 +0300

    таблица

--- page/sukharkov.php
+++ page/sukharkov.php
@@ -6,7 +6,15 @@
 <div class="block_text block-center">
     <div class="block_text-descriptions">
         <p><span class="tab fatty">Современный стиль</span>
-   создан для того, чтобы отдохнуть от поднадоевшей классики, от пафоса и «громоздкости», при этом не теряя уют и гармонию. Главная особенность современной кухни заключается в её практичности и функциональности. Она может быть выглядеть просто и лаконично, но иметь «навороченную» начинку. Внешний вид таких кухонь не подчинен каким-либо дизайнерским канонам оформления, он может сочетать в себе черты нескольких современных стилей: минимализма, модерна, хай-тек, скандинавского, и других.  Главное, чтобы интерьер выглядел стильно и создавал уютную атмосферу «невесомости» пространства. Обычно это ощущение достигается за счет того, что фасады имеют гладкую поверхность – никаких массивных выступающих линий, «тяжелого люкса» и прочих извращенных инсинуаций. Современный стиль, как и всё гениальное, прост. Дверцы открываются от обычного нажатия рукой, имеют либо интегрированные ручки, либо ручки простой геометрической формы.
+            создан для того, чтобы отдохнуть от поднадоевшей классики, от пафоса и «громоздкости», при этом не теряя уют
+            и гармонию. Главная особенность современной кухни заключается в её практичности и функциональности. Она
+            может быть выглядеть просто и лаконично, но иметь «навороченную» начинку. Внешний вид таких кухонь не
+            подчинен каким-либо дизайнерским канонам оформления, он может сочетать в себе черты нескольких современных
+            стилей: минимализма, модерна, хай-тек, скандинавского, и других. Главное, чтобы интерьер выглядел стильно и
+            создавал уютную атмосферу «невесомости» пространства. Обычно это ощущение достигается за счет того, что
+            фасады имеют гладкую поверхность – никаких массивных выступающих линий, «тяжелого люкса» и прочих
+            извращенных инсинуаций. Современный стиль, как и всё гениальное, прост. Дверцы открываются от обычного
+            нажатия рукой, имеют либо интегрированные ручки, либо ручки простой геометрической формы.
 
         </p>
     </div>
@@ -19,153 +27,144 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_1948.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_1948.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_1948.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_1982.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_1982.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_1982.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_1990.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_1990.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_1990.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_1992.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_1992.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_1992.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2017.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2017.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2017.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2042.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2042.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2042.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2049.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2049.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2049.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2057.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2057.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2057.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2071.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2071.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2071.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2079.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2079.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2079.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2105.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2105.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2105.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2130.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2130.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2130.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2133.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2133.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2133.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2137.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2137.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2137.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2157.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2157.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2157.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2169.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2169.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2169.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2174.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2174.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2174.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/sukharkov/IMG_2179.jpg">
-                  <img class="img-fluid" src="/img/sukharkov/IMG_2179.jpg">
+                    <img class="img-fluid" src="/img/sukharkov/IMG_2179.jpg">
                 </a>
             </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-        <table class="w-100 table table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl" border="1">
-            <tr>
-                <td>
-                    <p class="text-center">Год завершения проекта</p>
-                </td>
-                <td>
+        <div class="w-100 table">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Год завершения проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">2019</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Место проекта</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Место проекта</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">г. Москва</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Стиль</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Стиль</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Современная классика</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Тип планировки</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Тип планировки</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">П-образная с полуостровом</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
+                </div>
+
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Ключевые особенности и задействованные материалы</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <ul>
                         <li>Фурнитура марки BLUM</li>
                         <li>фасады: классика, МДФ, матовая эмаль, стиль «шейкер»</li>
-                        <li>столешница: акриловый камень </li>
+                        <li>столешница: акриловый камень</li>
                         <li>фартук: плитка «кабанчик»</li>
-                        <li>встроенная техника </li>
+                        <li>встроенная техника</li>
                         <li>монолитная раковина</li>
                     </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Дизайнер</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Елена Яковлева</p>
-                </td>
-            </tr>
-        </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
