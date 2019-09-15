a4afddf09ba7f39784fee222bf2f975b4741105c
sergejslepenkov <Segasle@gmail.com>
2019-09-15 10:12:51 +0300

    таблица

--- page/cookers.php
+++ page/cookers.php
@@ -6,7 +6,13 @@
 <div class="block_text block-center">
     <div class="block_text-descriptions">
         <p><span class="tab fatty">Современный стиль с элементами модерна</span>
-             – это то, что нужно для любителей стильных и функциональных кухонь. Модерн – один из самых сложных современных стилей, ведь он вобрал в себя черты и классики, и минимализма, и хай-тек.  Кухня в таком дизайне отличается, в первую очередь, лаконичностью и удобством. Как правило, она имеет гладкие поверхности в сочетании с древесными текстурами, оборудована встроенной современной техникой и выдержана в монохромной гамме приглушенных холодных оттенков. Изюминкой является то, что такой дизайн допускает аутентичные элементы декора: растительность в горшках, искусственные фрукты, имитация больших столовых приборов на стене. Проект, представленный ниже, не оставит равнодушными всех любителей стильного и функционального интерьера!
+            – это то, что нужно для любителей стильных и функциональных кухонь. Модерн – один из самых сложных
+            современных стилей, ведь он вобрал в себя черты и классики, и минимализма, и хай-тек. Кухня в таком дизайне
+            отличается, в первую очередь, лаконичностью и удобством. Как правило, она имеет гладкие поверхности в
+            сочетании с древесными текстурами, оборудована встроенной современной техникой и выдержана в монохромной
+            гамме приглушенных холодных оттенков. Изюминкой является то, что такой дизайн допускает аутентичные элементы
+            декора: растительность в горшках, искусственные фрукты, имитация больших столовых приборов на стене. Проект,
+            представленный ниже, не оставит равнодушными всех любителей стильного и функционального интерьера!
 
         </p>
     </div>
@@ -19,92 +25,92 @@
         <div class="row">
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8219.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8219.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8219.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8227.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8227.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8227.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8238.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8238.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8238.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8250.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8250.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8250.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8255.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8255.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8255.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8273.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8273.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8273.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8278-2.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8278-2.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8278-2.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8278.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8278.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8278.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8283.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8283.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8283.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8310.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8310.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8310.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8314.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8314.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8314.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8324.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8324.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8324.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8327.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8327.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8327.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8333.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8333.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8333.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8342.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8342.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8342.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8346.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8346.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8346.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8207.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8207.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8207.jpg">
                 </a>
             </div>
             <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-3 pt-4">
                 <a data-fancybox="gallery" href="/img/cookers/IMG_8184.jpg">
-                  <img class="img-fluid" src="/img/cookers/IMG_8184.jpg">
+                    <img class="img-fluid" src="/img/cookers/IMG_8184.jpg">
                 </a>
             </div>
         </div>
@@ -134,36 +140,39 @@
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center fatty">Тип планировки</p>
                 </div>
-                <td>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">П-образная </p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Ключевые особенности и задействованные материалы</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Ключевые особенности и задействованные материалы</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <ul>
                         <li>Фурнитура марки BLUM</li>
-                        <li>фасады: для верхнего яруса - МДФ, с интегрированными ручками, серая матовая эмаль; для нижнего яруса – шпонированный МДФ, сшитый с массивом дуба и покрытый морилкой для сохранения натурального цвета дерева (массив идет в местах фрезеровки интегрированных ручек для того, чтобы сохранялась прочность конструкции, и чтобы текстура дерева была даже в местах фрезеровки)</li>
-                        <li>столешница: акриловый камень </li>
+                        <li>фасады: для верхнего яруса - МДФ, с интегрированными ручками, серая матовая эмаль; для
+                            нижнего яруса – шпонированный МДФ, сшитый с массивом дуба и покрытый морилкой для сохранения
+                            натурального цвета дерева (массив идет в местах фрезеровки интегрированных ручек для того,
+                            чтобы сохранялась прочность конструкции, и чтобы текстура дерева была даже в местах
+                            фрезеровки)
+                        </li>
+                        <li>столешница: акриловый камень</li>
                         <li>фартук: мрамор</li>
                         <li>техника встроена в корпус мебели</li>
                         <li>монолитная двойная раковина</li>
                         <li>проведено светодиодное освещение по низу верхних модулей</li>
-                        <li>имеется барная зона, где в стеклянном шкафчике расположены розетки, которые выдвигаются при нажатии)</li>
+                        <li>имеется барная зона, где в стеклянном шкафчике расположены розетки, которые выдвигаются при
+                            нажатии)
+                        </li>
                     </ul>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="text-center">Дизайнер</p>
-                </td>
-                <td>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
+                    <p class="text-center fatty">Дизайнер</p>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 border">
                     <p class="text-center">Елена Яковлева</p>
-                </td>
-            </tr>
+                </div>
+            </div>
         </div>
-    </div> </div>
+    </div>
 </div>
 
